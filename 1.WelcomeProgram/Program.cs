835699baa8962b33ab40b39e3a9486feb641fb74
SriharshaShesham <sriharsha@sheshams.in>
2021-01-07 19:52:44 -0500

    Adding Welcome, Scopes and Conventions Code

---
+++ 1.WelcomeProgram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System; //Library / Reference
+
+
+//Namespace
+namespace _1.WelcomeProgram
+{
+    //Class
+    class Program
+    {
+        
+        //Function / Method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from main head!");
+            ECE();
+            CSE();
+        }
+
+        //Function / Method
+        static void CSE()
+        {
+            Console.WriteLine("Hello from  CSE");
+        }
+
+        //Function / Method
+        static void ECE()
+        {
+            Console.WriteLine("Hello from  ECE");
+        }
+    }
+}
---
+++ 2.ScopesAndConventions/Program.cs
@@ -0,0 +1,37 @@
+﻿// 1. Camel Case (starting small letter followed by Caps letter for all the words)
+// 1. Pascal Case (Caps letter for all the words)
+
+
+using System;
+
+namespace _2.ScopesAndConventions
+{
+    //Class Name Pascal case
+    class Program 
+    {
+        //Method Name Pascal case
+        static void Main(string[] args)
+        {
+            //variable declaration Camel Case (starting small letter followed by Caps letter for all the words)
+            int firstNumberToAdd = 3;
+            int secondNumberToAdd = 4;
+            const float Pi=3.14f;
+
+            {
+                int thirdNumberToAdd=6;
+
+                {
+                    int fourthNumberToAdd=7;
+                }
+
+                
+            }
+
+            Console.WriteLine(firstNumberToAdd);
+            Console.WriteLine(secondNumberToAdd);
+            Console.WriteLine(thirdNumberToAdd);
+            Console.WriteLine(fourthNumberToAdd);
+
+        }
+    }
+}

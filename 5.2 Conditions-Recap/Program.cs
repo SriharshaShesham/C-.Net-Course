766bcd04304c0afba594dd23e1582a3374dd735f
SriharshaShesham <sriharsha@sheshams.in>
2021-04-06 21:21:24 -0400

    5.2 Condition Recap

---
+++ 5.2 Conditions-Recap/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _5._2_Conditions_Recap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName="";
+            string lastName="";
+
+            System.Console.WriteLine("Enter user first name");
+            firstName=Console.ReadLine();
+            System.Console.WriteLine("Enter user last name");
+            lastName=Console.ReadLine();
+            
+            
+            if(CheckIfStringIsEmpty(firstName))//if(true/false)
+                System.Console.WriteLine("Invalid first name");
+            if(CheckIfStringIsEmpty(lastName))
+                System.Console.WriteLine("Invalid last name");
+
+
+        }
+
+        static bool CheckIfStringIsEmpty(string inputString)//
+        {
+            if(inputString=="")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}

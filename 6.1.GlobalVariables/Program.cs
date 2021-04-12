8a1b2b8f90a39233a0820212c12464cdb4d1569d
SriharshaShesham <sriharsha@sheshams.in>
2021-04-11 21:10:57 -0400

    7. Switch

---
+++ 6.1.GlobalVariables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _6._1.GlobalVariables
+{
+    class Program
+    {
+        static int userStartingBalance=1000;
+        static const int minimumBalance=100;
+        
+        static void Main(string[] args)
+        {
+            
+            Withdraw(100);
+            System.Console.WriteLine(userStartingBalance);
+        }
+
+        static void Withdraw(int amount)
+        {
+            userStartingBalance+=amount;     // userStartingBalance = userStartingBalance + amount;
+               
+        }
+
+        
+    }
+}
---
+++ 7.SwitchCases/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace _7.SwitchCases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte userChoice = 0;
+
+            System.Console.WriteLine("1. Morning Greeting");
+            System.Console.WriteLine("2. Afternoon Greeting");
+            System.Console.WriteLine("3. Evening Greeting");
+            System.Console.WriteLine("4. Night Greeting");
+
+            System.Console.WriteLine("Select your option");
+            userChoice = byte.Parse(Console.ReadLine());
+
+
+
+
+
+            // ---------------------- Using switch ----------------------------
+
+            
+            switch (userChoice)
+            {
+                case 1:
+                    {
+                        System.Console.WriteLine("Good Morning Sir");
+                        System.Console.WriteLine("The weather is 32 degree celcius");
+                    }
+                    break;
+
+                case 2:
+                    {
+                        System.Console.WriteLine("Good Afternoon sir");
+                        System.Console.WriteLine("Its your lunch time");
+                    }
+                    break;
+
+                case 3:
+                    {
+                        System.Console.WriteLine("Good evening sir");
+                        System.Console.WriteLine("Its time to leave for home");
+                        System.Console.WriteLine("The traffic is moderate. The time to home is 1 hr");
+                    }
+
+                default:
+                    {
+                        System.Console.WriteLine("Sir! Its time for bed");
+                        System.Console.WriteLine("Here is your night playlist");
+                        System.Console.WriteLine("Good night sir");
+                    }
+                    break;
+            }
+
+            // ---------------------- End using switch ----------------------------
+
+        }
+
+        static void usingIfElse(byte userChoice)
+        {
+            // ---------------------- Using if else ----------------------------
+
+            if (userChoice == 1)
+            {
+                System.Console.WriteLine("Good Morning Sir");
+                System.Console.WriteLine("The weather is 32 degree celcius");
+            }
+
+            else if (userChoice == 2)
+            {
+                System.Console.WriteLine("Good Afternoon sir");
+                System.Console.WriteLine("Its your lunch time");
+            }
+
+            else if (userChoice == 3)
+            {
+                System.Console.WriteLine("Good evening sir");
+                System.Console.WriteLine("Its time to leave for home");
+                System.Console.WriteLine("The traffic is moderate. The time to home is 1 hr");
+            }
+
+            else
+            {
+                System.Console.WriteLine("Sir! Its time for bed");
+                System.Console.WriteLine("Here is your night playlist");
+                System.Console.WriteLine("Good night sir");
+            }
+
+
+            // ---------------------- End of using if else ----------------------------
+        }
+    }
+}

cbabee5e739d53a2229aecb39b711a8dd0c5c680
SriharshaShesham <sriharsha@sheshams.in>
2021-05-02 21:32:58 -0400

    ClassesAndProperties

---
+++ 1. Basics/10.1.ClassessAndProperties/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ClassessAndProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] arrayName = new int[3];
+            Student[] students = new Student[3];
+            
+            arrayName[0]=1;
+
+            students[0]=new Student();
+            students[0].StudentID=1234;
+            
+            students[1]=new Student();
+            students[1].StudentID=1234;
+
+            students[2]=new Student();
+            students[2].StudentID=1234;
+
+            for (int counter = 0; counter < students.Length; counter++)
+            {
+                System.Console.WriteLine(students[counter].StudentID);
+            }
+
+            
+            
+        }
+    }
+
+    class Student
+    {
+        int _studentID;
+        string _studentFirstName;
+        string _studentLastName;
+        char _gender;
+
+        public int StudentID
+        {
+            get
+            {
+                return _studentID;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _studentID = value;
+                }
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return _studentFirstName;
+            }
+            set
+            {
+                _studentFirstName = value;
+            }
+        }
+
+    }
+
+}
--- 1. Basics/9.Classess/Program.cs
+++ 1. Basics/9.Classess/Program.cs
@@ -13,9 +13,7 @@ namespace _9.Classess
 
             //TO access user class create an object.
             UserClass userObj=new UserClass();
-
-            userObj.SetUserName("Krishna");
-            System.Console.WriteLine(userObj.userName);
+            userObj.userName="Test";
 
             // int age=GetUserAge();
             // System.Console.WriteLine($"User age is {age}");
@@ -26,20 +24,8 @@ namespace _9.Classess
 
     class UserClass
     {
-        // int[] tempArray= new int[10];
-        private string userName=""; 
-        private int userAge=-1;
-
-        public string GetUserName()
-        {
-            return userName;
-        }
-
-        public void SetUserName(string inputUserName)
-        {
-            if(inputUserName!="")
-                userName=inputUserName;
-        }
+        public string userName=""; 
+        public int userAge=-1;
 
     }
 }

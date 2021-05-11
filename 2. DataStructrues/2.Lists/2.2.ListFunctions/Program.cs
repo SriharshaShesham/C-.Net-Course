3eae68cee3d0ee4df9c04c0416123323c90171a8
SriharshaShesham <sriharsha@sheshams.in>
2021-05-10 21:16:38 -0400

    List Task

--- 2. DataStructrues/2.Lists/2.2.ListFunctions/Program.cs
+++ 2. DataStructrues/2.Lists/2.2.ListFunctions/Program.cs
@@ -10,15 +10,28 @@ namespace _2._2.ListFunctions
     {
         static void Main(string[] args)
         {
+
+            int[] newItemsToAdd = new int[4];
+
+            newItemsToAdd[0]=20;
+            newItemsToAdd[1]=30;
+            newItemsToAdd[2]=40;
+            newItemsToAdd[3]=50;
+
             List<int> numberList = new List<int>();
             numberList.Add(1);
             numberList.Add(2);
             numberList.Add(3);
             numberList.Add(4);
             numberList.Add(5);
+            numberList.Add(5);
             numberList.Add(6);
+            numberList.Add(7);
+            numberList.Add(5);
+            numberList.Add(8);
+            numberList.Add(9);
 
-            PrintList(numberList);
+            // PrintList(numberList);
 
             // System.Console.WriteLine("List Remove function");
             // numberList.Remove(3);
@@ -31,12 +44,36 @@ namespace _2._2.ListFunctions
             // PrintList(numberList);
             // System.Console.WriteLine();
 
+            
 
-            System.Console.WriteLine("List Count function");
+            // System.Console.WriteLine("List Count function");
 
-            System.Console.WriteLine("List Count is " + numberList.Count);
-            System.Console.WriteLine();
+            // System.Console.WriteLine("List Count is " + numberList.Count);
+            // System.Console.WriteLine();
 
+
+            // System.Console.WriteLine("List Capacity function");
+
+            // System.Console.WriteLine("List Capacity is " + numberList.Capacity);
+            // System.Console.WriteLine();
+            
+            
+            // System.Console.WriteLine("List Contains function");
+
+            // System.Console.WriteLine("List Contains 9 " + numberList.Contains(9));
+            // System.Console.WriteLine("List Contains 19 " + numberList.Contains(19));
+            // System.Console.WriteLine();
+
+            // System.Console.WriteLine("List FindAll function");
+            foreach(int val in numberList.FindAll(num => num.Equals(5)))
+            {
+                System.Console.WriteLine(val);
+            }
+
+
+            numberList.InsertRange(5,newItemsToAdd);
+            
+            PrintList(numberList);
 
         }
 
@@ -52,28 +89,29 @@ namespace _2._2.ListFunctions
         }
     }
 
-    // class Temp
-    // {
-    //     private int _size = 0;
+    class Temp
+    {
+        int[] temArray= new int[6];
+        private int _size = 0;
 
-    //     public int Count
-    //     {
-    //         get
-    //         {
-    //             return _size;
-    //         }
-    //     }
+        public int Count
+        {
+            get
+            {
+                return _size;
+            }
+        }
 
-    //     public void Add(int number)
-    //     {
-    //         //current index
-    //         //add to list
-    //         _size++;
-    //     }
+        public void Add(int number)
+        {
+            //current index
+            //add to list
+            _size++;
+        }
 
-    //     public void Remove(int number)
-    //     {
-    //         _size--;
-    //     }
-    // }
+        public void Remove(int number)
+        {
+            _size--;
+        }
+    }
 }
---
+++ 2. DataStructrues/2.Lists/2.3.SchoolDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2._3.SchoolDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

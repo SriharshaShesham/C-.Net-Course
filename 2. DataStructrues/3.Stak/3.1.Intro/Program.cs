0c83801461695ea7b1bcb4bbd901fe6ab3423d39
SriharshaShesham <sriharsha@sheshams.in>
2021-05-13 22:25:14 -0400

    4. SocialNetworkingTask

---
+++ 2. DataStructrues/3.Stak/3.1.Intro/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._1.Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> numStack=new Stack<int>();
+            //Add
+            //Remove
+            //Clear
+            //Contains
+
+            numStack.Push(1);
+            numStack.Push(2);
+            numStack.Push(3);
+            numStack.Push(4);
+            numStack.Push(5);
+            numStack.Push(6);
+
+            PrintTheStack(numStack);
+
+            System.Console.WriteLine("Number being Popped is "+numStack.Pop());
+
+            PrintTheStack(numStack);
+
+            System.Console.WriteLine($"Contains: {numStack.Contains(5)}");
+
+            PrintTheStack(numStack);
+
+            System.Console.WriteLine("Peak: "+numStack.Peek());;
+
+            System.Console.WriteLine("Count: "+numStack.Count);;
+
+            numStack.Clear();
+
+
+            
+            PrintTheStack(numStack);
+            
+            // System.Console.WriteLine("Peak: "+numStack.Peek());;
+
+
+            string inputStr;
+
+            System.Console.WriteLine("Enter a string");
+            inputStr=Console.ReadLine();
+
+            Stack<char> inStack = new Stack<char>();
+            
+            
+            foreach(char inChar in inputStr) //  [a,b,c,d,e] -> Collection of Chars -> 
+                inStack.Push(inChar);//Adding
+
+            foreach(char inChar in inputStr)
+                System.Console.WriteLine(inStack.Pop());
+
+            // string name="Rama";
+            // for(int Count=0;Count<name.Length;Count++)
+            // {
+            //     System.Console.WriteLine(name[Count]);
+            // }
+
+
+
+        }
+
+        static void PrintTheStack(Stack<int> stackToPrint)
+        {
+            System.Console.WriteLine("Printing");
+            foreach(int num in stackToPrint)
+                System.Console.WriteLine(num);
+        }
+    }
+}
---
+++ 2. DataStructrues/4.Queues/4.1.Intro/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._1.Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numQueue=new Queue<int>();
+            //Add
+            //Remove
+            //Clear
+            //Contains
+
+            numQueue.Enqueue(1);
+            numQueue.Enqueue(2);
+            numQueue.Enqueue(3);
+            numQueue.Enqueue(4);
+            numQueue.Enqueue(5);
+            numQueue.Enqueue(6);
+
+            PrintTheQueue(numQueue);
+
+
+            System.Console.WriteLine("Number being DeQueued is "+numQueue.Dequeue());;
+
+            PrintTheQueue(numQueue);
+
+        }
+        static void PrintTheQueue(Queue<int> QueueToPrint)
+        {
+            System.Console.WriteLine("Printing");
+            foreach(int num in QueueToPrint)
+                System.Console.Write(num+"   |   ");
+        }
+    }
+
+    class MyQueue
+    {
+        List<int> myQueue= new List<int>();
+
+        public void Add(int number)
+        {
+            myQueue.Add(number);
+        }
+
+        public int Delete()
+        {
+            int numberToBeDeleted= myQueue[0];
+            myQueue.RemoveAt(0);
+            return numberToBeDeleted;
+        }
+    }
+    
+}

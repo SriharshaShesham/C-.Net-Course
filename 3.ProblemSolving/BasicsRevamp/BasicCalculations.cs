fa4a6c2b36f4332166b1e07f6e7c1ba295b936e0
Sriharsha Shesham <sriharsha@sheshams.in>
2021-09-08 07:42:03 +0530

    Basics Revamp

---
+++ 3.ProblemSolving/BasicsRevamp/BasicCalculations.cs
@@ -0,0 +1,19 @@
+BasicCalculations
+{
+    Addition(int num1, int num2)
+    {
+            // ValidationCheck
+    }
+    Substration(int num1, int num2)
+    {
+        // ValidationCheck
+    }
+    Multiplication(int num1, int num2)
+    {
+        // ValidationCheck
+    }
+    Division(int num1, int num2)
+    {
+        // ValidationCheck
+    }
+}
---
+++ 3.ProblemSolving/BasicsRevamp/Program.cs
@@ -0,0 +1,20 @@
+Program
+{
+    void main()
+    {
+        System.Console.WriteLine("Basic calculations");
+        basicCalculations();
+    }
+
+    void basicCalculations()
+    {
+        1.Addition
+        2.Substration. Substract 2nd Number from first 
+        3.Multiplication. Multiply 1st number by second
+        4.Division. Divide 1st number by second
+        
+        Enter num1
+        Enter num2
+        
+    }
+}
